6c9d63919b939111fab2b5186cea78bbac37bec2
frontweb66 <frontweb66@gmail.com>
2025-09-22 19:49:12 +0800

    feat: 修改baseurl

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    base: '/bwi/', // 必须设置为仓库名称
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
     },
-  },
 })
